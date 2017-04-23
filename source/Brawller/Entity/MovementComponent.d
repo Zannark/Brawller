397951a383ef9227c4ffe5100f5b48def2c800b5
Zannark <sherbet.gray54@googlemail.com>
2017-04-23 16:04:57 +0100

    Documentation

--- source/Brawller/Entity/MovementComponent.d
+++ source/Brawller/Entity/MovementComponent.d
@@ -3,6 +3,9 @@ module Brawller.Entity.MovementComponent;
 import dsfml.system;
 import Brawller.Entity.BaseEntity;
 
+/++
+ + The base class for all movement, AI and/or player.
+ ++/
 abstract class MovementComponent
 {
 	public void UpdateMovement(BaseEntity Entity, float dt);
